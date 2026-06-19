75109d754e86ff2c1f84c16c691db3a91d80286f
Adapt Technology <sistemas@adaptechnology.biz>
2026-06-19 10:25:07 -0400

    fix(staff): sincronizar datos de sesion en modulo de personal

--- app/Controllers/StaffController.php
+++ app/Controllers/StaffController.php
@@ -2,9 +2,25 @@
 
 namespace Controllers;
 
+use Core\Session;
+
 class StaffController{
 
     public function index(){
+
+        if(!Session::has('id_user')){
+            header("Location: /login");
+            exit;
+        }
+
+        $cedula = Session::get('cedula');
+
+        $last_connection_raw = Session::get('last_connection');
+        $last_connection = '';
+
+        if (!empty($last_connection_raw)) {
+            $last_connection = date('d/m/Y - h:i a', strtotime($last_connection_raw));
+        }
 
         require_once __DIR__ . '/../View/staff/index.php';
     }
